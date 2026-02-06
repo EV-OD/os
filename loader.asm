402aa2890c8925a9d893955cd3a2063ac5ffce89
EV-OD <evod599@gmail.com>
2026-02-06 22:17:54 +0545

    Refactor loader: replace loader.s with loader.asm and update linker script to include multiboot header

--- loader.asm
+++ loader.asm
@@ -4,11 +4,14 @@ MAGIC_NUMBER  equ 0x1BADB002
 FLAG equ 0x0
 CHECKSUM equ -(MAGIC_NUMBER + FLAG)
 
-section .text
+section .multiboot_header
 align 4
     dd MAGIC_NUMBER
     dd FLAG
     dd CHECKSUM
+
+section .text
+align 4
 
 extern kmain
 
