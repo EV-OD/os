00b0c7e234d27ed5a63ef14fe37de3cfd81eed99
EV-OD <evod599@gmail.com>
2026-02-06 12:30:07 +0545

    Update .gitignore, add GRUB configuration, and enhance build script for ISO generation

--- runner.sh
+++ runner.sh
@@ -9,3 +9,17 @@ nasm -f elf32 loader.asm
 # -T link.ld specifies the linker script to use, which defines how the sections of the object file should be arranged in memory
 # -melf_i386 specifies the target architecture as 32-bit Intel x86
 ld -T link.ld -melf_i386 loader.o -o kernel.elf
+
+
+
+
+genisoimage -R \
+    -b boot/grub/stage2_eltorito \
+    -no-emul-boot \
+    -boot-load-size 4 \
+    -A os \
+    -input-charset utf8 \
+    -quiet \
+    -boot-info-table \
+    -o os.iso \
+    iso
