eb0c9dea95aca94610899b7f207333135c876c94
EV-OD <evod599@gmail.com>
2026-02-06 22:20:09 +0545

    Refactor loader: replace loader.asm with loader.s and update CMakeLists.txt for assembly compilation

--- loader.s
+++ loader.s
@@ -4,14 +4,13 @@ MAGIC_NUMBER  equ 0x1BADB002
 FLAG equ 0x0
 CHECKSUM equ -(MAGIC_NUMBER + FLAG)
 
-section .multiboot_header
+section .text
 align 4
     dd MAGIC_NUMBER
     dd FLAG
     dd CHECKSUM
 
-section .text
-align 4
+
 
 extern kmain
 
